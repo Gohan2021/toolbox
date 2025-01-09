af234cbedd922c8301ad6d4e13c89ef15e1be154
Nicolas.M. <nicoomorenoo@gmail.com>
2025-01-09 14:23:08 -0500

    Cambios en las carpetas On branch main Your branch is up to date with 'origin/main'. Changes to be committed: deleted:    pagina.zip modified:   pagina/app/index.js

--- pagina/app/index.js
+++ pagina/app/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mysql  from "mysql2";
 //fix para el dirname
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -15,6 +16,23 @@ app.listen(app.get("port"), () => {
 //configuracion 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
+
+// Create a connection to the database
+const db = mysql.createConnection({
+    host: '156.67.74.251', // Your MySQL host
+    user: 'u880664821_toolbox', // Your MySQL username
+    password: 'T00lB0xDB4dm1n!', // Your MySQL password
+    database: 'u880664821_toolbox' // Your database name
+});
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
 
 //rutas
 app.get("/", (req, res) => res.sendFile(__dirname + "/pages/index.html"));
