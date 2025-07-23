73151ab59b304e0c3197747162d0c46d0a8457e8
Nicolas.M. <nicoomorenoo@gmail.com>
2025-07-23 00:32:52 -0500

    correción de  login aliado

--- pagina/app/database.js
+++ pagina/app/database.js
@@ -1,34 +1,29 @@
-// database.js
 import mysql from 'mysql2/promise';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Crear un pool de conexiones a la base de datos
 const pool = mysql.createPool({
     host: '156.67.74.251',
-    user: process.env.USER, 
-    password: process.env.PASSWORD, 
-    database: process.env.DATABASE, 
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    charset: "utf8mb4" // Para evitar el error de 'cesu8'
-}); 
+    charset: 'utf8mb4'
+});
 
-// Obtener una conexión del pool
-const getConnection = async () => {
-    return await pool.getConnection();
-};
+// ⚡ La función devuelve el pool directamente
+const database = async () => pool;
 
-// Cerrar todas las conexiones del pool
 export const closePool = async () => {
     try {
         await pool.end();
-        console.log("Conexiones a la base de datos cerradas.");
+        console.log('Conexiones a la base de datos cerradas.');
     } catch (error) {
-        console.error("Error al cerrar las conexiones del pool:", error.message);
+        console.error('Error al cerrar el pool:', error.message);
     }
 };
 
-export default getConnection;
+export default database;
